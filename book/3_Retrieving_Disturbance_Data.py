98fd854ce95cf32a24b9668b6cfe4447336a7e77
Karthik Venkataramani <karthikvt@vt.edu>
2024-05-29 16:36:43 -0700

    first completed draft of disturbance script

--- book/3_Retrieving_Disturbance_Data.py
+++ book/3_Retrieving_Disturbance_Data.py
@@ -17,50 +17,22 @@
 # In particular, we will be examining the area around the city of [Alexandroupolis](https://en.wikipedia.org/wiki/Alexandroupolis) which was severely impacted by the wildfires, resulting in loss of lives, property, and forested areas.
 
 # +
-# Notebook dependencies
-# Imports for plotting
-import geoviews as gv
-gv.extension('bokeh')
-gv.output(size=400)
-
-import hvplot.xarray, hvplot.pandas
-import holoviews as hv
-from holoviews.plotting.util import process_cmap
-hv.extension('bokeh')
-
-# Matplotlib imports
+# Plotting imports
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
-import contextily as cx
 from rasterio.plot import show
 from mpl_toolkits.axes_grid1.anchored_artists import AnchoredSizeBar
 
-import hvplot.xarray
-import geoviews as gv
-import geopandas as gpd
-import holoviews as hv
-import panel.widgets as pnw
-import datetime
-from datetime import datetime
-
-from bokeh.models import FixedTicker
-hv.extension('bokeh')
-
-import warnings
-warnings.filterwarnings('ignore')
-
-import sys
-
 # GIS imports
-from shapely.geometry import Point, box
+from shapely.geometry import Point
 from osgeo import gdal
 from rasterio.merge import merge
 import rasterio
+import contextily as cx
 
 # data wrangling imports
-import numpy as np
 import pandas as pd
-import xarray as xr
+import numpy as np
 
 # misc imports
 from datetime import datetime, timedelta
@@ -75,28 +47,11 @@ gdal.SetConfigOption('GDAL_HTTP_COOKIEJAR', '~/cookies.txt')
 gdal.SetConfigOption('GDAL_DISABLE_READDIR_ON_OPEN','EMPTY_DIR')
 gdal.SetConfigOption('CPL_VSIL_CURL_ALLOWED_EXTENSIONS','TIF, TIFF')
 
-from dist_utils import stack_bands, time_and_area_cube
-# -
-
-# ---
-# ## **Data Information Input**
-
-# In the code cell below, the user should specify the:
-# * Start and end days of interest <br>
-# * Date iteration step<br>
-# * Data directory<br>
-# * Band list<br>
-# * Anomaly threshold<br><br>
-#
-# **<font color='red'>Note: The cell below is the only code in the notebook that should be modified. </font>**
-#
-
 # +
 # Define data search parameters
 
 # Define AOI as left, bottom, right and top lat/lon extent
-# aoi = box(22.93945, 40.69742,25.57617, 41.68221)
-dadia_forest = Point(26.18798, 41.08474).buffer(0.1)
+dadia_forest = Point(26.18, 41.08).buffer(0.1)
 
 # We will search data for the month of March 2024
 start_date = datetime(year=2023, month=8, day=1)
@@ -153,29 +108,96 @@ granules.head()
 refined_granules = defaultdict(list)
 
 for i, row in granules.iterrows():
-    refined_granules[i.strftime('%y-%m-%d')].append(row.hrefs)
+    refined_granules[i.strftime('%Y-%m-%d')].append(row.hrefs)
 
 refined_granules = pd.DataFrame(index=refined_granules.keys(), data = {'hrefs':refined_granules.values()})
 
 # +
 # The wildfire near Alexandroupolis started on August 21st and rapidly spread, particularly affecting the nearby Dadia Forest
 # For demonstration purposes, let's look at three dates to study the extent of the fire - 
-# August 1st, August 25th, and September 1st
+# August 1st, August 25th, and September 19th
 # We will plot the OPERA-DIST-ALERT data product, highlighting only those pixels corresponding to confirmed vegetation damage,
 # and in particular only those pixels where at least 50% of the area was affected (layer value 6)
+
+dates_of_interest = [datetime(year=2023, month=8, day=1), datetime(year=2023, month=8, day=26), datetime(year=2023, month=9, day=18)]
+hrefs_of_interest = [x.hrefs for i, x in refined_granules.iterrows() if datetime.strptime(i, '%Y-%m-%d') in dates_of_interest]
 # -
 
-# **Layer Values:**
+# **Relevant layer Values for DIST-ALERT product:**
 #
-# **These need updating since the DIST product update in early 2024**
 # * **0:** No disturbance<br>
-# * **1:** Provisional (**first detection**) Disturbance with vegetation cover change <50% <br>
-# * **2:** Confirmed (**recurrent detection**) Disturbance with vegetation cover change < 50% <br> 
-# * **3:** Provisional Disturbance with vegetation cover change ≥ 50% <br>
-# * **4:** Confirmed Disturbance with vegetation cover change ≥ 50%  <br> 
+# * **1:** First detection of disturbance with vegetation cover change <50% <br>
+# * **2:** Provisional detection of disturbance with vegetation cover change <50% <br>
+# * **3:** Confirmed detection of disturbance with vegetation cover change <50% <br>
+# * **4:** First detection of disturbance with vegetation cover change >50% <br>
+# * **5:** Provisional detection of disturbance with vegetation cover change >50% <br>
+# * **6:** Confirmed detection of disturbance with vegetation cover change >50% <br>
 
-# ---
-# ## **Summary** 
-# In this Jupyter notebook, we showcase how end-users can leverage the **OPERA Land Surface Disturbance (DIST)** product **to interactively visualize the evolution and calculate the extent of a wildfire**.
-# <br>
-# ***
+# +
+# Define color map to generate plot (Red, Green, Blue, Alpha)
+colors = [(1, 1, 1, 0)] * 256  # Initial set all values to white, with zero opacity
+colors[6] = (1, 0, 0, 1)       # Set class 6 to Red with 100% opacity
+
+# Create a ListedColormap
+cmap = ListedColormap(colors)
+
+# +
+fig, ax = plt.subplots(1, 3, figsize = (30, 10))
+crs = None
+
+for i, (date, hrefs) in enumerate(zip(dates_of_interest, hrefs_of_interest)):
+    
+    # Read the crs to be used to generate basemaps
+    if crs is None:
+        with rasterio.open(hrefs[0]) as ds:
+            crs = ds.crs
+
+    if len(hrefs) == 1:
+        with rasterio.open(hrefs[0]) as ds:
+            raster = ds.read()
+            transform = ds.transform
+    else:
+        raster, transform = merge(hrefs)
+
+    show(raster, ax=ax[i], transform=transform, interpolation='none')
+    cx.add_basemap(ax[i], crs=crs, zoom=9, source=cx.providers.OpenStreetMap.Mapnik)
+    show(raster, ax=ax[i], transform=transform, interpolation='none', cmap=cmap)
+
+    scalebar = AnchoredSizeBar(ax[i].transData,
+                            10000 , '10 km', 'lower right', 
+                            color='black',
+                            frameon=False,
+                            pad = 0.25,
+                            sep=5,
+                            fontproperties = {'weight':'semibold', 'size':12},
+                            size_vertical=300)
+
+    ax[i].add_artist(scalebar)
+    ax[i].ticklabel_format(axis='both', style='scientific',scilimits=(0,0),useOffset=False,useMathText=True)
+    ax[i].set_xlabel('UTM easting (meters)')
+    ax[i].set_ylabel('UTM northing (meters)')
+    ax[i].set_title(f"Disturbance extent on: {date.strftime('%Y-%m-%d')}")
+# -
+
+# Next, let's calculate the area extent of damage over time
+
+# +
+damage_area = []
+conversion_factor = (30*1e-3)**2 # to convert pixel count to area in km^2; each pixel is 30x30 meters
+
+# this will take a few minutes to run, since we are retrieving data for multiple days
+for index, row in refined_granules.iterrows():
+    raster, transform = merge(row.hrefs)
+    damage_area.append(np.sum(raster==6)*conversion_factor)
+
+refined_granules['damage_area'] = damage_area
+
+# -
+
+fig, ax = plt.subplots(1, 1, figsize=(20, 10))
+ax.plot([datetime.strptime(i, '%Y-%m-%d') for i in refined_granules.index], refined_granules['damage_area'], color='red')
+ax.grid()
+plt.ylabel('Area damaged by wildfire (km$^2$)', size=15)
+plt.xlabel('Date', size=15)
+plt.xticks([datetime(year=2023, month=8, day=1) + timedelta(days=6*i) for i in range(11)], size=14)
+plt.title('2023 Dadia forest wildfire detected extent', size=14)
