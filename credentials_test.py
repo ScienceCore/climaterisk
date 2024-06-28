52c72c794bcd76f973a6327add44795631e1e3df
Dhavide Aruliah <dhavide@gmail.com>
2024-06-28 12:14:33 -0700

    Updates credentials_test.py

--- credentials_test.py
+++ credentials_test.py
@@ -1,53 +1,35 @@
-# minimum test needed to see if credentials are set correctly to download data from NASA Earthdata
-# Users need to have a .netrc file in their home directory with valid credentials for this script to work
+# Minimal test to verify NASA Earthdata credentials for downloading data products.
+# Requires a .netrc file in home directory containing valid credentials.
 
+import osgeo.gdal, rasterio
 from pystac_client import Client
-from datetime import datetime
-from shapely.geometry import Point
-import rasterio
-from osgeo import gdal
-import warnings
+from warnings import filterwarnings
+filterwarnings("ignore") # suppress PySTAC warnings
 
-# suppress PySTAC warnings
-warnings.filterwarnings("ignore")
+# Mandatory GDAL setup for accessing cloud data
+osgeo.gdal.SetConfigOption('GDAL_HTTP_COOKIEFILE','~/.gdal_cookies.txt')
+osgeo.gdal.SetConfigOption('GDAL_HTTP_COOKIEJAR', '~/.gdal_cookies.txt')
+osgeo.gdal.SetConfigOption('GDAL_DISABLE_READDIR_ON_OPEN','EMPTY_DIR')
+osgeo.gdal.SetConfigOption('CPL_VSIL_CURL_ALLOWED_EXTENSIONS','TIF, TIFF')
 
-# GDAL setup for accessing cloud data
-gdal.SetConfigOption('GDAL_HTTP_COOKIEFILE','~/cookies.txt')
-gdal.SetConfigOption('GDAL_HTTP_COOKIEJAR', '~/cookies.txt')
-gdal.SetConfigOption('GDAL_DISABLE_READDIR_ON_OPEN','EMPTY_DIR')
-gdal.SetConfigOption('CPL_VSIL_CURL_ALLOWED_EXTENSIONS','TIF, TIFF')
+# Define AOI (Area-Of-Interest) & time-window
+livingston_tx, delta = (-95.09, 30.69), 0.1
+aoi = tuple(coord + sgn*delta for sgn in (-1,+1) for coord in livingston_tx)
+time_window =  '2024-04-30/2024-05-31'
 
-STAC_URL = 'https://cmr.earthdata.nasa.gov/stac'
-
-catalog = Client.open(f'{STAC_URL}/POCLOUD/') 
-
-# Setup PySTAC client
-provider_cat = Client.open(STAC_URL)
+# Prepare PySTAC client
+STAC_URL, collections = 'https://cmr.earthdata.nasa.gov/stac', ["OPERA_L3_DSWX-HLS_V1"]
 catalog = Client.open(f'{STAC_URL}/POCLOUD/')
-collections = ["OPERA_L3_DSWX-HLS_V1"]
 
-start_date = datetime(year=2024, month=4, day=30)
-stop_date = datetime(year=2024, month=5, day=31)
-livingston_tx = Point(-95.09, 30.69)
-date_range = f'{start_date.strftime("%Y-%m-%d")}/{stop_date.strftime("%Y-%m-%d")}'
-
-opts = {
-    'bbox' : livingston_tx.buffer(0.01).bounds, 
-    'collections': collections,
-    'datetime' : date_range,
-}
-
-# Execute the search
-print("Testing PySTAC search ..")
-search = catalog.search(**opts)
-results = list(search.items_as_dicts())
-print(f"Search succesful, accessing test data ..")
-
-test_href = results[0]['assets']['0_B01_WTR']['href']
+print("Testing PySTAC search...")
+opts = dict(bbox=aoi, collections=collections, datetime=time_window)
+results = list(catalog.search(**opts).items_as_dicts())
+test_uri = results[0]['assets']['0_B01_WTR']['href']
 
 try:
-    with rasterio.open(test_href) as ds:
-        _ = ds.profile
-        print("Success! Your credentials file is correctly configured!")
+    print(f"Search successful, accessing test data...")
+    with rasterio.open(test_uri) as ds: _ = ds.profile
+    print("Success! Your credentials file is correctly configured!")
 except:
-    print("Could not access data, ensure that the .netrc file exists in the user home directory and contains valid NASA Earthdata credentials")
+    print(f"Could not access NASA EarthData asset: {test_uri}")
+    print("Ensure that a .netrc file containing valid NASA Earthdata credentials exists in the user home directory.")
